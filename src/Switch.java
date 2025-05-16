d6b336e7a87dc324683c97b81bf3fc58031b34c4
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2025-05-15 21:21:14 -0300

    Enhaced switch

---
+++ src/Switch.java
@@ -0,0 +1,18 @@
+public class Switch {
+    public static void main(String[]args){
+
+        String day = "S";
+
+        switch (day){
+            case "Monday" -> System.out.println("It is a weekday");
+            case "Tuesday" -> System.out.println("It is a weekday");
+            case "Wednesday" -> System.out.println("It is a weekday");
+            case "Thursday" -> System.out.println("It is a weekday");
+            case "Friday" -> System.out.println("It is a weekday");
+            case "Saturday" -> System.out.println("It is a weekend");
+            case "Sunday" -> System.out.println("It is a weekend");
+
+            default -> System.out.println(day + " Is not a day!");
+        }
+    }
+}
