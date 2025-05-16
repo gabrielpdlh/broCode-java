06395f6419a27ca67a1b85373cfeb166750ffab3
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2025-05-15 21:21:50 -0300

    Simple calculator

---
+++ src/Calculator.java
@@ -0,0 +1,35 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[]args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        double firstValue;
+        String operator;
+        double secondValue;
+        double result = 0;
+
+        System.out.print("Enter the firstValue: ");
+        firstValue = scanner.nextDouble();
+
+        System.out.print("Please choose an operator (+, -, *, /)");
+        operator = scanner.next();
+
+        System.out.print("Enter the firstValue: ");
+        secondValue = scanner.nextDouble();
+
+        switch (operator){
+            case "+" -> result = firstValue + secondValue;
+            case "-" -> result = firstValue - secondValue;
+            case "*" -> result = firstValue * secondValue;
+            case "/" -> result = firstValue / secondValue;
+
+            default -> System.out.println("Choose a valid operator!");
+
+        }
+
+        System.out.printf("The result is: %.2f", result);
+    }
+}
