9fec9d3f3cf442c61ec8825c8e43b473ffdde4aa
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2025-06-19 16:31:24 -0300

    feat: var ags

---
+++ src/VarArgs.java
@@ -0,0 +1,17 @@
+public class VarArgs {
+    public static void main(String[] args){
+
+        System.out.println(average(1,2,3,4));
+    }
+
+    static double average(double... numbers){
+
+        double sum = 0;
+
+        for(double number : numbers){
+            sum =+ number;
+        }
+
+        return sum / numbers.length;
+    }
+}
