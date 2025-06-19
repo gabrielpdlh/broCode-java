589cf429d5f77d8ef94035e59940f33658884da8
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2025-06-19 16:31:18 -0300

    feat: inherit example

---
+++ src/POO/Animal.java
@@ -0,0 +1,14 @@
+package POO;
+
+public class Animal {
+
+    boolean isAlive;
+
+    Animal(){
+         isAlive= true;
+    }
+
+    void eat(){
+        System.out.println("The animal is eating");
+    }
+}
---
+++ src/POO/Main.java
@@ -0,0 +1,10 @@
+package POO;
+
+public class Main {
+    public static void main(String[]args){
+
+        Cat cat = new Cat();
+
+        cat.eat();
+    }
+}
