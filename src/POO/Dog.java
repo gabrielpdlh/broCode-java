8e7a9c03560ea1f06e5eb36f9293816f805e3248
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2025-06-20 23:36:40 -0300

    feat: dog POO example

---
+++ src/POO/Dog.java
@@ -0,0 +1,5 @@
+package POO;
+
+public class Dog extends Animal {
+
+}
--- src/POO/Main.java
+++ src/POO/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[]args){
 
         Cat cat = new Cat();
+        Dog dog = new Dog();
 
         cat.eat();
+        dog.eat();
     }
 }
