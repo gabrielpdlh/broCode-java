bba79b4e63ba9ed78a4823fc3e5f9b9fe508e923
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2025-06-29 23:21:48 -0300

    feat: hipo

--- src/POO/Hipo.java
+++ src/POO/Hipo.java
@@ -1,4 +1,15 @@
 package POO;
 
 public class Hipo extends Animal{
+    public void sound() {
+        System.out.println("O hipopótamo grunhe: 'Grrrrrrrr!'");
+    }
+
+    public void swim() {
+        System.out.println("O hipopótamo está nadando no rio.");
+    }
+
+    public void openMouth() {
+        System.out.println("O hipopótamo abre um bocão enorme!");
+    }
 }
