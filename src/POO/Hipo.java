a3da8f4a0dff333542ad1ab26f02cedb341b3434
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2025-06-30 23:55:02 -0300

    feat: hipo methods

--- src/POO/Hipo.java
+++ src/POO/Hipo.java
@@ -1,6 +1,9 @@
 package POO;
 
-public class Hipo extends Animal{
+import java.util.Random;
+
+public class Hipo extends Animal {
+
     public void sound() {
         System.out.println("O hipopótamo grunhe: 'Grrrrrrrr!'");
     }
@@ -11,5 +14,34 @@ public class Hipo extends Animal{
 
     public void openMouth() {
         System.out.println("O hipopótamo abre um bocão enorme!");
+    }
+
+    public void rest() {
+        System.out.println("O hipopótamo está descansando à sombra.");
+    }
+
+    public void eat() {
+        System.out.println("O hipopótamo está comendo capim às margens do rio.");
+    }
+
+    public void dailyRoutine() {
+        System.out.println("🌅 Início da rotina do hipopótamo:");
+
+        swim();
+        if (new Random().nextBoolean()) {
+            sound();
+        } else {
+            openMouth();
+        }
+
+        eat();
+
+        int horas = new Random().nextInt(3) + 1;
+        for (int i = 1; i <= horas; i++) {
+            System.out.println("Hora " + i + " de descanso...");
+            rest();
+        }
+
+        System.out.println("🌇 Fim da rotina do hipopótamo.\n");
     }
 }
